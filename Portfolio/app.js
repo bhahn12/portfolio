f70fcc6e98f67117204fd79bd9d7376a048c532f
bhahn12 <68400995+bhahn12@users.noreply.github.com>
2021-06-29 11:11:11 -0400

    Add files via upload

---
+++ Portfolio/app.js
@@ -0,0 +1,9 @@
+//add some kind of meat to intro before about
+//animations for heading
+//fix skills
+//center project pictures
+//color scheme
+
+$(document).ready(function () {
+    $('div.hidden').fadeIn(2000);  
+});
